990cf3232b0a8197672a449c6a7bb45fc7632db4
zoodogood <zoodogood@gmail.com>
2022-10-31 08:57:16 +0200

    Refactoring

--- index.js
+++ index.js
@@ -16,10 +16,10 @@ const preloadPath = path.join(__dirname, 'preload.js');
 app.whenReady().then(async () => {
     const results = await dialog.showOpenDialog({ properties: ['openFile', 'openDirectory'] });
     let pathDir = results.filePaths[0];
-    let jsonFiles = [];
 
-    function searchFile(dir){
+    function findPackagesJson(dir){
         const files = [ dir ];
+        const packagesJson = [];
         const blackList = require("#constants/blackList.js");
 
         while (files.length){
@@ -35,7 +35,7 @@ app.whenReady().then(async () => {
 
                 const packageJson = filesList.find(fileName => fileName === "package.json");
                 if (packageJson){
-                    jsonFiles.push(`${ file }/${ packageJson }`);
+                    packagesJson.push(`${ file }/${ packageJson }`);
                     continue;
                 }
 
@@ -44,52 +44,21 @@ app.whenReady().then(async () => {
             }
             
         }
+
+        return packagesJson;
     }
 
-    
-    searchFile(pathDir);
-    console.log(jsonFiles);
+    const ProjectData = require("#classes/ProjectData");
+    const projectsData = findPackagesJson(pathDir)
+        .map(path => new ProjectData(path));
 
-    let names = [];
-    let versions = [];
-    let dependencies = [];
-    let devDependencies =[];
-    let mainfiles = [];
-    let descriptions = [];
+    projectsData.forEach(project => project.loadSync());
 
-    for (let i = 0; i < jsonFiles.length; i++){
-        let read = fs.readFileSync(jsonFiles[i]);
-        let jsonRead = JSON.parse(read);
-        names.push(jsonRead.name);
-        versions.push(jsonRead.version);
-        mainfiles.push(jsonRead.main)
-        if(!(jsonRead.dependencies)){
-            dependencies.push('None');
-        }else{
-            dependencies.push(jsonRead.dependencies)
-        }
-        if(!(jsonRead.devDependencies)){
-            devDependencies.push('None')
-        }else{
-            devDependencies.push(jsonRead.devDependencies)
-        }
-        if(!(jsonRead.description)){ 
-            descriptions.push('Unspecified'); 
-        }else{
-            descriptions.push(jsonRead.description);
-        }
-    }
 
     const win = createWindow({indexHTMLPath, preloadPath});
     win.toggleDevTools();
 
-    ipcMain.handle('names', () => names);
-    ipcMain.handle('versions', () => versions);
-    ipcMain.handle('dependencies', () => dependencies);
-    ipcMain.handle('mainfiles', () => mainfiles);
-    ipcMain.handle('devDependencies', () => devDependencies);
-    ipcMain.handle('descriptions', () => descriptions);
-
+    ipcMain.handle('projects', () => projectsData);
 });
 
 app.on('window-all-closed', () => {
