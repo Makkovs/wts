86f10d85bb8a47615b12e0b0980cf9bbd781231b
zoodogood <zoodogood@gmail.com>
2022-10-31 09:08:49 +0200

    Add bubble title on hover

--- public/js/add.js
+++ public/js/add.js
@@ -4,15 +4,26 @@
     for (const project of projects){
         const data = project.data;
 
-        
-        document.getElementById('container').innerHTML +=
+        const dependencies = data.dependencies ?
+            Object.keys(data.dependencies).join(", ") : "void";
+
+        const devDependencies = data.devDependencies ?
+            Object.keys(data.devDependencies).join(", ") : "void";
+
+        const container = document.getElementById('container');
+        container.innerHTML +=
         `
-            <div class="element"><big>Name:</big> <span>${data.name}</span> 
-            <big>Versions:</big> <span>${data.version}</span> <br>
-            <big>Main file:</big> <span>${data.main}</span> <br>
-            <big>Dependencies:</big><span>${data.dependencies ?? "void"}</span> <br>
-            <big>devDependencies:</big><span>${data.devDependencies ?? "void"}</span> <br>
-            <big>Description:</big> <span>${data.description}</span> </div>
+            <aside>
+                <div class="element"><big>Name:</big> <span>${data.name}</span> 
+                <big>Versions:</big> <span>${data.version}</span> <br>
+                <big>Main file:</big> <span>${data.main}</span> <br>
+                <big>Dependencies:</big><span>${dependencies}</span> <br>
+                <big>devDependencies:</big><span>${devDependencies}</span> <br>
+                <big>Description:</big> <span>${data.description}</span> </div>
+            </aside>
         `
+
+        const node = container.querySelector('aside:last-child');
+        node.setAttribute('title', project.path);
     }
 })();
