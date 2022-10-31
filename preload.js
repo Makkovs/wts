e50e51d5d4243cbd7173bbd345da1ceb13b3be7d
zoodogood <zoodogood@gmail.com>
2022-10-31 09:00:22 +0200

    Variables moved to data object

--- preload.js
+++ preload.js
@@ -1,10 +1,5 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('params', {
-  names: () => ipcRenderer.invoke('names'),
-  versions: () => ipcRenderer.invoke('versions'),
-  dependencies: () => ipcRenderer.invoke('dependencies'),
-  mainfiles: () => ipcRenderer.invoke('mainfiles'),
-  devDependencies: () => ipcRenderer.invoke('devDependencies'),
-  descriptions: () => ipcRenderer.invoke('descriptions')
+  projects: () => ipcRenderer.invoke('projects')
 });
--- public/js/add.js
+++ public/js/add.js
@@ -1,29 +1,18 @@
-const func = async () => {
-    const names = await params.names();
-    const versions = await params.versions();
-    const dependencies = await params.dependencies();
-    const mainfiles = await params.mainfiles();
-    const devDependencies = await params.devDependencies();
-    const descriptions = await params.descriptions();
-    for (let i = 0; i < names.length; i++){
-        let dependencies$ = []; let devDependencies$ = []
-        if (dependencies[i] != 'None'){
-            for (let j in dependencies[i]){
-                dependencies$.push(' ' + j);
-                console.log(j)
-            }
-        }else {dependencies$.push('None')}
-        if (devDependencies[i] != 'None'){
-            for (let j in devDependencies[i]){
-                devDependencies$.push(' ' + j);
-            }
-        }else {devDependencies$.push('None')}
-        document.getElementById('container').innerHTML += `<div class="element"><big>Name:</big> <span>${names[i]}</span> 
-        <big>Versions:</big> <span>${versions[i]}</span> <br>
-        <big>Main file:</big> <span>${mainfiles[i]}</span> <br>
-        <big>Dependencies:</big><span>${dependencies$}</span> <br>
-        <big>devDependencies:</big><span>${devDependencies$}</span> <br>
-        <big>Description:</big> <span>${descriptions[i]}</span> </div>`
+(async () => {
+    const projects = await params.projects();
+
+    for (const project of projects){
+        const data = project.data;
+
+        
+        document.getElementById('container').innerHTML +=
+        `
+            <div class="element"><big>Name:</big> <span>${data.name}</span> 
+            <big>Versions:</big> <span>${data.version}</span> <br>
+            <big>Main file:</big> <span>${data.main}</span> <br>
+            <big>Dependencies:</big><span>${data.dependencies ?? "void"}</span> <br>
+            <big>devDependencies:</big><span>${data.devDependencies ?? "void"}</span> <br>
+            <big>Description:</big> <span>${data.description}</span> </div>
+        `
     }
-}
-func()
+})();
